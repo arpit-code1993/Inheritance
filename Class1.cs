4dc4f5be5244d8a98271d38fabff1b42add4596f
arpit-code1993 <arpitgupta@mail.usf.edu>
2019-09-24 22:12:41 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Class1
+    {
+
+        public int salary;
+        public string name;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program : Class1
+    {
+        static void Main(string[] args)
+        {
+            Class1 cs = new Class1();
+            cs.name="Arpit Gupta";
+            cs.salary = 100000;
+
+            Console.WriteLine("name {0}", cs.name);
+            Console.WriteLine("salary {0}", cs.salary);
+        }
+    }
+}
